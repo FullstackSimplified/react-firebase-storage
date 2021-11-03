5c7177812033b98857836562bf645ae4f6d3059b
Prasoon <Prasoon@Prasoons-MacBook-Pro.local>
2021-11-03 17:00:48 +0530

    Updated to firebase v9

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import { useState } from "react";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { storage } from "./firebase";
 
 function App() {
+  const [progress, setProgress] = useState(0);
   const formHandler = (e) => {
     e.preventDefault();
     const file = e.target[0].files[0];
+    uploadFiles(file);
+  };
+
+  const uploadFiles = (file) => {
+    //
+    if (!file) return;
+    const sotrageRef = ref(storage, `files/${file.name}`);
+    const uploadTask = uploadBytesResumable(sotrageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const prog = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(prog);
+      },
+      (error) => console.log(error),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+        });
+      }
+    );
   };
 
   return (
@@ -13,6 +40,7 @@ function App() {
         <button type="submit">Upload</button>
       </form>
       <hr />
+      <h2>Uploading done {progress}%</h2>
     </div>
   );
 }
