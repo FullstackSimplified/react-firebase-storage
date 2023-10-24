fd1bd53925e8e7d409202a4ca3403637f5df10fb
Prasoon <Prasoon@Prasoons-MacBook-Pro.local>
2023-10-24 12:15:40 +0530

    feat: image display grid

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { storage } from "./firebase";
 
 function App() {
   const [progress, setProgress] = useState(0);
+  const [images, setImages] = useState([
+    "https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1698019200&semt=sph",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWg6cEGOkqkDOldajm1-JBfA4p7d34qALSNpdCrBfDqg&s",
+  ]);
   const formHandler = (e) => {
     e.preventDefault();
     const file = e.target[0].files[0];
@@ -29,6 +33,7 @@ function App() {
           .getDownloadURL()
           .then((url) => {
             console.log(url);
+            setImages((prevState) => [...prevState, url]);
           });
       }
     );
@@ -42,6 +47,14 @@ function App() {
       </form>
       <hr />
       <h2>Uploading done {progress}%</h2>
+      {/* an image grid */}
+      <div className="image-container">
+        {images.map((img, index) => (
+          <section key={index} className="image-wrapper">
+            <img src={img} alt="" width="100%" height="100%" />
+          </section>
+        ))}
+      </div>
     </div>
   );
 }
